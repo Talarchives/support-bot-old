c42e774d0df0228394353f979260eca4ee815bac
Talal <talalahmad257@gmail.com>
2021-01-01 14:46:08 +0500

    Updated logs command

--- src/commands/dev/logs.js
+++ src/commands/dev/logs.js
@@ -6,39 +6,16 @@ class logsCommand extends Command {
       category: 'dev',
       aliases: ['logs', 'log'],
       description: {
-        content: 'Get logs',
-        usage: '[timestamp]',
-        examples: ['1609320948099']
-      },
-      args: [
-        {
-          id: 'timestamp',
-          prompt: {
-            optional: true
-          },
-          default: msg => new Date().getTime() - msg.client.uptime
-        }
-      ]
+        content: 'Get logs'
+      }
     });
   }
 
-  async exec(msg, { timestamp }) {
-    fs.readdir('./logs/', 'utf8', (err, files) => {
-      if(err) return msg.reply(`❌ Could not read the directory!\`\`\`js\n${err}\`\`\``);
-      let file, hs = 0;
-      for (let i = 0; i < files.length; i++) {
-        const curr = files[i];
-        const similarity = this.client.util.similarity(curr, timestamp.toString());
-        if(similarity > hs) {
-          hs = similarity;
-          file = curr;
-        }
-      }
-      fs.readFile(`./logs/${file}`, 'utf8', async (err, data) => {
-        if(err) return msg.reply(`❌ Could not read the file!\`\`\`js\n${err}\`\`\``);
-        const haste = await this.client.util.haste(data);
-        msg.channel.send(haste);
-      });
+  async exec(msg) {
+    fs.readFile('./logs/.log', 'utf8', async (err, data) => {
+      if(err) return msg.reply(`❌ Could not read the file!\`\`\`js\n${err}\`\`\``);
+      const haste = await this.client.util.haste(data);
+      msg.channel.send(haste);
     });
   }
 }

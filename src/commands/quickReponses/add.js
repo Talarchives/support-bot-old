6eb43ce3da4d4d0093c1b1de27dfca262c5108f8
Talal <talalahmad257@gmail.com>
2021-01-01 16:00:38 +0500

    Added aqr command

---
+++ src/commands/quickReponses/add.js
@@ -0,0 +1,66 @@
+const { Command } = require('discord-akairo');
+class aqrCommand extends Command {
+  constructor() {
+    super('aqr', {
+      category: 'Quick Response',
+      userPermissions: 'MANAGE_MESSAGES',
+      clientPermissions: 'EMBED_LINKS',
+      aliases: ['addQuickResponse', 'aqr', 'aq'],
+      description: {
+        content: 'Add a quick response',
+        usage: '<name> <response>',
+        examples: [
+          'cool You are cool!'
+        ]
+      },
+      args: [
+        {
+          id: 'name',
+          type: 'uniqueQR',
+          prompt: {
+            start: 'Enter a quick response name.',
+            retry: '❌ A quick response with that name or alias already exists. Please enter a unique name.'
+          }
+        },
+        {
+          id: 'response',
+          match: 'rest',
+          prompt: {
+            start: 'Enter the response.'
+          }
+        },
+        {
+          id: 'aliases',
+          match: 'none',
+          type: 'uniqueQR',
+          prompt: {
+            infinite: true,
+            start: 'Enter aliases in *separate* messages\nType `stop` when you\'re done',
+            retry: (_, { phrase: val }) => {
+              if (val.toLowerCase() === 'stop') return '❌ You must enter at least 1 alias (can be same as the name)\nType `stop` when you\'re done';
+              return '❌ A quick response with that name or alias already exists. Please enter unique aliases.\nType `stop` when you\'re done';
+            }
+          }
+        }
+      ]
+    });
+  }
+
+  async exec(msg, args) {
+    let qr = await this.client.settings.get(msg.guild.id, 'quickResponses', []);
+    qr = qr.concat(args);
+    await this.client.settings.set(msg.guild.id, 'quickResponses', qr);
+    const embed = msg.client.util.embed()
+      .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+      .setColor(process.env.color)
+      .addFields([
+        { name: 'Name', value: args.name, inline: true },
+        { name: 'Aliases', value: args.aliases.map(a => `\`${a}\``).join(', '), inline: true },
+        { name: 'Response', value: args.response }
+      ]);
+    return msg.reply('✅ Quickresponse Added!', embed);
+  }
+}
+
+
+module.exports = aqrCommand;

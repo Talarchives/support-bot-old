690c5264cdc73f447cefe6760d6b4ec589682ffb
Talal <talalahmad257@gmail.com>
2021-01-02 11:01:22 +0500

    Added embed description

--- src/commands/quickReponses/list.js
+++ src/commands/quickReponses/list.js
@@ -26,6 +26,7 @@ class lqrCommand extends Command {
 
   async exec(msg, args) {
     const embed = msg.client.util.embed()
+      .setDescription(`Use command \`${msg.util.parsed.prefix}lq <name/alias>\` to it's response and aliases`)
       .setColor(process.env.color)
       .setAuthor(msg.author.tag, msg.author.displayAvatarURL());
     if (args && args.qr) {

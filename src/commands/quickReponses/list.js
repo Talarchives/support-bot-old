1bdad43dcc1081746da35dcabc8103b4a76caf39
Talal <talalahmad257@gmail.com>
2021-01-02 09:56:59 +0500

    Fixed issues coming from use cmd

--- src/commands/quickReponses/list.js
+++ src/commands/quickReponses/list.js
@@ -24,23 +24,23 @@ class lqrCommand extends Command {
     });
   }
 
-  async exec(msg, { qr }) {
+  async exec(msg, args) {
     const embed = msg.client.util.embed()
       .setColor(process.env.color)
       .setAuthor(msg.author.tag, msg.author.displayAvatarURL());
-    if (qr) {
+    if (args && args.qr) {
       embed
         .addFields([
-          { name: 'Name', value: qr.name, inline: true },
-          { name: 'Aliases', value: qr.aliases.map(a => `\`${a}\``).join(', '), inline: true },
-          { name: 'Response', value: qr.response }
+          { name: 'Name', value: args.qr.name, inline: true },
+          { name: 'Aliases', value: args.qr.aliases.map(a => `\`${a}\``).join(', '), inline: true },
+          { name: 'Response', value: args.qr.response }
         ]);
       return msg.channel.send(embed);
     } else {
       const qrs = await this.client.settings.get(msg.guild.id, 'quickResponses', []);
       if(!qrs.length) return msg.reply('❌ No quick responses found!');
       embed
-        .setTitle('Quick Responses')
+        .setTitle('List of Quick Responses')
         .setDescription(qrs.map(q => `\`${q.name}\``).join(', '));
       return msg.channel.send(embed);
     }
